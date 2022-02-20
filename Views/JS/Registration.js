35ee8e108bafbaa60b6ca925972c588a1cdf1a7c
PrzEllam <PerezAlex088@outlook.com>
2022-02-19 23:38:17 -0600

    Mejor UX

--- Views/JS/Registration.js
+++ Views/JS/Registration.js
@@ -3,23 +3,6 @@ var dateFormat = date.getFullYear() + '-' +
 String(date.getMonth() + 1).padStart(2, '0') + '-' + String(date.getDate()).padStart(2, '0');
 document.getElementById('date-of-birth').value = dateFormat;
 
-const pictureBox = document.getElementById('picture-box');
-
-const form = document.getElementById('register-form');
-const btnSubmit = document.getElementById('btn-submit');
-
-const showPassword = document.getElementById('show-password');
-const hidePassword = document.getElementById('hide-password');
-const inputPassword = document.getElementById('password');
-
-/*
-names.onclick = function() {
-    nameWarning.style.visibility = null;
-    nameErrorLabel.style.display = null;
-    names.style.border = null;
-    names.style.transition = null;
-}
-*/
 photo.onchange = function(e) {
 
     let fReader = new FileReader();
@@ -27,48 +10,222 @@ photo.onchange = function(e) {
     fReader.onloadend = function(e) {
         let img = document.getElementById('picture-box');
         img.setAttribute('src', e.target.result);
-        pictureBox.style.opacity = '1';
+        img.style.opacity = '1';
         photo.style.opacity = '0';
     }
 
 }
-/*
-photo.onclick = function() {
 
-    let fReader = new FileReader();
-    fReader.readAsDataURL(photo.files[0]);
-    fReader.onloadend = function(e) {
-        let img = document.getElementById('picture-box');
-        img.setAttribute('src', e.target.result);
-        //img.src = e.target.result;
+// Solo letras del alfabeto
+var rgxAlphas = /^[a-zA-Z \u00C0-\u00FF]+$/;
+
+// Solo letras del alfabeto y numeros
+var rgxAlphaNum = /^[a-zA-Z0-9 \u00C0-\u00FF]+$/;
+
+// Solo hay espacios en blanco
+var rgxWhitespaces = /^\s*$/; 
+
+// Validar formato de email
+let rgxEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+
+
+function validateName(input) {
+
+    if (input.value === '') {
+        setMessageError(input, 'Nombre(s) no puede estar vacío.');
+        return 1;
     }
 
+    if (!input.value.match(rgxAlphas) || input.value.match(rgxWhitespaces)) {
+        setMessageError(input, 'Nombre(s) no válido.');
+        return 1;
+    }
+
+    setMessageSuccess(input);
+    return 0;
+
 }
-*/
-form.addEventListener('submit', (e) => {
+
+function validateLastName(input) {
+
+    if (input.value === '') {
+        setMessageError(input, 'Apellidos no puede estar vacío.');
+        return 1;
+    }
+
+    if (!input.value.match(rgxAlphas) || input.value.match(rgxWhitespaces)) {
+        setMessageError(input, 'Apellidos no válido.');
+        return 1;
+    }
+    
+    setMessageSuccess(input);
+    return 0;
+
+}
+
+function validateDateOfBirth(input) {
+
+    if (input.value === '') {
+        setMessageError(input, 'Fecha de nacimiento no puede estar vacío.');r
+        return 1;
+    }
+
+    if (Date.parse(dateFormat) < Date.parse(input.value)) {
+        setMessageError(input, 'Fecha de nacimiento no válida.');
+        return 1;
+    }
+
+    setMessageSuccess(input);
+    return 0;
+
+}
+
+function validateEmail(input) {
+
+    if (input.value === '') {
+        setMessageError(input, 'Correo electrónico no puede estar vacío.');
+        return 1;
+    }
+
+    else if (!input.value.match(rgxEmail) || input.value.match(rgxWhitespaces)) {
+        setMessageError(input, 'Correo electrónico no válido.');
+        return 1;
+    }
+
+    setMessageSuccess(input);
+    return 0;
+
+}
+
+function validateUsername(input) {
+
+    if (input.value === '') {
+        setMessageError(input, 'Nombre de usuario no puede estar vacío.');
+        return 1;
+    }
+    if (!input.value.match(rgxAlphas) || input.value.match(rgxWhitespaces)) {
+        setMessageError(input, 'Nombre de usuario no válido.');
+        return 1;
+    }
+    
+    setMessageSuccess(input);
+    return 0;
+
+}
+
+function validatePassword(input) {
+
+    if (input.value === '') {
+        setMessageError(input, 'Contraseña no puede estar vacía.');
+        let upper = document.getElementById('field-password-upper');
+        let lower = document.getElementById('field-password-lower');
+        let number = document.getElementById('field-password-number');
+        let symbol = document.getElementById('field-password-symbol');
+        let length = document.getElementById('field-password-length');
+        upper.style.color = 'rgb(222, 79, 84)';
+        lower.style.color = 'rgb(222, 79, 84)';
+        number.style.color = 'rgb(222, 79, 84)';
+        symbol.style.color = 'rgb(222, 79, 84)';
+        length.style.color = 'rgb(222, 79, 84)';
+        return 1;
+    }
+
+    if (!checkPassword(input.value)) {
+        setMessageError(input, 'Contraseña no válida.');
+        return 1;
+    }
+    
+    setMessageSuccess(input);
+    return 0;
+
+}
+
+function validateConfirmPassword(input) {
+
+    if (input.value === '') {
+        setMessageError(input, 'Contraseña no puede estar vacía.');
+        return 1;
+    }
+    if (document.getElementById('password').value != input.value) {
+        setMessageError(input, 'Confirmar contraseña no coincide con contraseña.');
+        return 1;
+    }
+
+    setMessageSuccess(input);
+    return 0;
+
+}
+
+
+document.getElementById('first-name').addEventListener('blur', function() {
+    validateName(this);
+});
+
+document.getElementById('first-name').addEventListener('focus', function() {
+    onFocus(this);
+});
+
+document.getElementById('last-name').addEventListener('blur', function() {
+    validateLastName(this);
+});
+
+document.getElementById('last-name').addEventListener('focus', function() {
+    onFocus(this);
+});
+
+document.getElementById('date-of-birth').addEventListener('blur', function() {
+    validateDateOfBirth(this); 
+});
+
+document.getElementById('date-of-birth').addEventListener('focus', function() {
+    onFocus(this);
+});
+
+document.getElementById('email').addEventListener('blur', function() {
+    validateEmail(this)
+});
+
+document.getElementById('email').addEventListener('focus', function() {
+    onFocus(this);
+});
+
+document.getElementById('username').addEventListener('blur', function() {
+    validateUsername(this);
+});
+
+document.getElementById('username').addEventListener('focus', function() {
+    onFocus(this);
+});
+
+document.getElementById('password').addEventListener('blur', function() {
+    validatePassword(this)
+});
+
+document.getElementById('password').addEventListener('focus', function() {
+    onFocus(this);
+});
+
+document.getElementById('confirm-password').addEventListener('blur', function() {
+    validateConfirmPassword(this);
+});
+
+document.getElementById('confirm-password').addEventListener('focus', function() {
+    onFocus(this);
+});
+
+
+document.getElementById('register-form').addEventListener('submit', (e) => {
 
     // Obtain inputs
-    let photo = form['photo'];
-    let name = form['name'];
-    let lastName = form['last-name'];
-    let dateOfBirth = form['date-of-birth'];
-    let email = form['email'];
-    let username = form['username'];
-    let password = form['password'];
-    let confirmPassword = form['confirm-password'];
-
-    // Solo letras del alfabeto
-    var rgxAlphas = /^[a-zA-Z \u00C0-\u00FF]+$/;
-
-    // Solo letras del alfabeto y numeros
-    var rgxAlphaNum = /^[a-zA-Z0-9 \u00C0-\u00FF]+$/;
-
-    // Solo hay espacios en blanco
-    var rgxWhitespaces = /^\s*$/; 
-
-    // Validar formato de email
-    let rgxEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    
+    let photo = this['photo'];
+    let name = this['first-name'];
+    let lastName = this['last-name'];
+    let dateOfBirth = this['date-of-birth'];
+    let email = this['email'];
+    let username = this['username'];
+    let password = this['password'];
+    let confirmPassword = this['confirm-password'];
 
     if(photo.value === ''){
         setMessageErrorOnPhoto();
@@ -76,79 +233,39 @@ form.addEventListener('submit', (e) => {
         setMessageSuccessOnPhoto();
     }
 
+    let result = 0;
+    result += validateName(name);
+    result += validateLastName(lastName);
+    result += validateDateOfBirth(dateOfBirth);
+    result += validateEmail(email);
+    result += validateUsername(username);
+    result += validatePassword(password);
+    result += validateConfirmPassword(confirmPassword);
 
-    if (name.value === '') {
-        setMessageError(name, 'Nombre(s) no puede estar vacío.');
-    }
-    else if (!name.value.match(rgxAlphas) || name.value.match(rgxWhitespaces)) {
-        setMessageError(name, 'Nombre(s) no válido.');
-    }
-    else {
-        setMessageSuccess(name);
-    }
-
-    if (lastName.value === '') {
-        setMessageError(lastName, 'Apellidos no puede estar vacío.');
-    }
-    else if (!lastName.value.match(rgxAlphas) || lastName.value.match(rgxWhitespaces)) {
-        setMessageError(name, 'Apellidos no válido.');
-    }
-    else {
-        setMessageSuccess(lastName);
+    if (result > 0) {
+        e.preventDefault();
     }
 
-    if(Date.parse(dateFormat) < Date.parse(dateOfBirth.value)){
-        setMessageError(dateOfBirth, 'Fecha de nacimiento no válida.');
-    }
-    else {
-        setMessageSuccess(dateOfBirth)
-    }
-
-    if (email.value === '') {
-        setMessageError(email, 'Correo electrónico no puede estar vacío.');
-    }
-    else if (!email.value.match(rgxEmail) || email.value.match(rgxWhitespaces)) {
-        setMessageError(email, 'Correo electrónico no válido.');
-    }
-    else {
-        setMessageSuccess(email);
-    }
-
-    if (username.value === '') {
-        setMessageError(username, 'Nombre de usuario no puede estar vacío.');
-    }
-    else if (!username.value.match(rgxAlphaNum) || lastName.value.match(rgxWhitespaces)) {
-        setMessageError(username, 'Nombre de usuario no válido.');
-    }
-    else {
-        setMessageSuccess(username);
-    }
-
-    if (password.value === '') {
-        setMessageError(password, 'Contraseña no puede estar vacío.');
-    }
-    else if (!validatePassword(password.value)) {
-        setMessageError(password, 'Contraseña no válida');
-    }
-    else {
-        setMessageSuccess(password);
-    }
-
-    if (confirmPassword.value === '') {
-        setMessageError(confirmPassword, 'Confirmar contraseña no puede estar vacío.');
-    }
-    else if (password.value != confirmPassword.value) {
-        setMessageError(confirmPassword, 'Confirmar contraseña no coincide con contraseña');
-    }
-    else {
-        setMessageSuccess(confirmPassword);
-    }
-
-    // Password
-    // let results = validatePassword(password);
-
-    e.preventDefault();
 });
+
+function setMessageSuccess(input) {
+
+    let fieldWrapper = input.parentElement.parentElement;
+
+    let fieldError = fieldWrapper.children[2];
+    let inputWarning = fieldWrapper.children[1].children[1];
+    let inputSucess = fieldWrapper.children[1].children[2];
+
+    fieldError.style.display = 'none';
+    fieldError.innerHTML = '';
+
+    inputWarning.style.visibility = 'hidden';
+    inputSucess.style.visibility = 'visible';
+
+    input.classList.add('input-correct');
+    input.classList.remove('input-incorrect');
+
+}
 
 function setMessageError(input, errorMessage) {
 
@@ -156,99 +273,87 @@ function setMessageError(input, errorMessage) {
 
     let fieldError = fieldWrapper.children[2];
     let inputWarning = fieldWrapper.children[1].children[1];
+    let inputSucess = fieldWrapper.children[1].children[2];
     
     fieldError.style.display = 'block';
     fieldError.innerHTML =  errorMessage;
+
     inputWarning.style.visibility = 'visible';
+    inputSucess.style.visibility = 'hidden';
+
     input.classList.add('input-incorrect');
+    input.classList.remove('input-correct');
 
 }
 
-function setMessageSuccess(input) {
+function onFocus(input) {
 
-    let inputWrapper = input.parentElement;
-    let inputSucess = inputWrapper.children[2];
+    let fieldWrapper = input.parentElement.parentElement;
 
-    inputSucess.style.visibility = 'visible';
-    input.classList.remove('input-incorrect');
+    let fieldError = fieldWrapper.children[2];
+    let inputWarning = fieldWrapper.children[1].children[1];
+    let inputSucess = fieldWrapper.children[1].children[2];
+
+    fieldError.style.display = 'none';
+    fieldError.innerHTML = '';
+
+    inputWarning.style.visibility = 'hidden';
+    inputSucess.style.visibility = 'hidden';
 
 }
 
+function checkPassword(password) {
 
-function validatePassword(password) {
-
-    let msg = 'Contraseña debe contener:';
     let result = true;
 
+    let upper = document.getElementById('field-password-upper');
+    let lower = document.getElementById('field-password-lower');
+    let number = document.getElementById('field-password-number');
+    let symbol = document.getElementById('field-password-symbol');
+    let length = document.getElementById('field-password-length');
+
     if (!password.match(/([A-Z])/)) {
-        let upper = document.getElementById('field-password-upper');
         upper.style.color = 'rgb(222, 79, 84)';
-        msg += ' una mayúscula';
         result = false;
     }
     else {
-        let upper = document.getElementById('field-password-upper');
         upper.style.color = 'rgb(121, 177, 143)';
     }
 
     if (!password.match(/([a-z])/)) {
-        let lower = document.getElementById('field-password-lower');
         lower.style.color = 'rgb(222, 79, 84)';
-        msg += ', una minúscula';
         result = false;
     }
     else {
-        let lower = document.getElementById('field-password-lower');
         lower.style.color = 'rgb(121, 177, 143)';
     }
 
     if (!password.match(/([0-9])/)) {
-        let number = document.getElementById('field-password-number');
         number.style.color = 'rgb(222, 79, 84)';
-        msg += ', un número';
         result = false;
     }
     else {
-        let number = document.getElementById('field-password-number');
         number.style.color = 'rgb(121, 177, 143)';
     }
 
     if (!password.match(/[.,\/#!$%\^&\*;:{}=\-_`~()”“"…]/)) {
-        let symbol = document.getElementById('field-password-symbol');
         symbol.style.color = 'rgb(222, 79, 84)';
-        msg += ", un símbolo";
         result = false;
     }
     else {
-        let symbol = document.getElementById('field-password-symbol');
         symbol.style.color = 'rgb(121, 177, 143)';
     }
 
     if (password.length < 8) {
-        let length = document.getElementById('field-password-length');
         length.style.color = 'rgb(222, 79, 84)';
-        msg += ' y un tamaño mínimo de 8 caracteres';
         result = false;
     }
     else {
-        let length = document.getElementById('field-password-length');
         length.style.color = 'rgb(121, 177, 143)';
     }
 
     return result;
 
-}
-
-showPassword.onclick = function() {
-    showPassword.style.visibility = 'hidden';
-    hidePassword.style.visibility = 'visible';
-    inputPassword.setAttribute('type', 'text');
-}
-
-hidePassword.onclick = function() {
-    showPassword.style.visibility = 'visible';
-    hidePassword.style.visibility = 'hidden';
-    inputPassword.setAttribute('type', 'password');
 }
 
 function setMessageErrorOnPhoto(){
